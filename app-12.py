c09ccbdbf66f92d46d0590808d3a82eb30792211
= <=>
2018-10-17 17:21:01 +0800

    18/10/17 modify

--- app-12.py
+++ app-12.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -7,26 +7,37 @@ app = Flask(__name__)
 
 app.config.from_object('config')
 
-# 连接数据库
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], max_overflow=5)
 
-# 创建类与表的映射
 Base = declarative_base()
+# 用户表
+class Users(Base):
+    __tablename__ = 'users'
+    # 类型需要导入
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(100), nullable=False)
 
-# 创建于数据库中表对应的模型类
 class Article(Base):
     __tablename__ = 'article'
     # 类型需要导入
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(100), nullable=False)
     content = Column(Text, nullable=False)
+    # 外键
+    author_id = Column(Integer, ForeignKey('users.id'))
 
-# 创建表
 Base.metadata.create_all(engine)
 
 
 @app.route('/')
 def index():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # 添加用户
+    user1 = Users(username='liu')
+    session.add(user1)
+    session.commit()
     return 'hello world'
 
 
--- config.py
+++ config.py
@@ -4,13 +4,13 @@ DEBUG = True,
 
 # 连接数据库配置
 # dialect + driver://username:password@host:port/database
-DIALECT = 'mysql',
-DRIVER = 'pymysql',
-USERNAME = 'root',
-PASSWORD = 'root',
-HOST = '127.0.0.1',
-PORT = '3306',
-DATABASE = 'db_demo1',
+DIALECT = 'mysql'
+DRIVER = 'pymysql'
+USERNAME = 'root'
+PASSWORD = 'root'
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'db_demo1'
 
 SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
