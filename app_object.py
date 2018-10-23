79ac2373284c329f8ebd2ded5ecc494257b3c8d3
flyingtothe <41361359+flyingtothe@users.noreply.github.com>
2018-10-23 22:13:08 +0800

    18/10/23 modify

--- app_object.py
+++ app_object.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -6,6 +6,13 @@ app.config.from_object('config')
 @app.route('/')
 def hello_world():
     return render_template('index8.html')
+
+@app.route('/login5/', methods=['GET', 'POST'])
+def login5():
+    if request.method == 'GET':
+        return render_template('login5.html')
+    else:
+        pass
 
 if __name__ == '__main__':
     app.run()
