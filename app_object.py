0110194a57f138302332f0ad9bc3573a4a7be2d8
= <=>
2018-10-25 17:13:41 +0800

    18/10/25 modify

--- app_object.py
+++ app_object.py
@@ -10,7 +10,11 @@ db.init_app(app)
 # 主页
 @app.route('/')
 def index8():
-    return render_template('index8.html')
+    context = {
+        # 时间排序由小到大加 " - "
+        'questions' : Question.query.order_by('-create_time').all()
+    }
+    return render_template('index8.html', **context)
 
 # 登陆
 @app.route('/login5/', methods=['GET', 'POST'])
@@ -77,6 +81,12 @@ def question():
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('index8'))
+
+# 查看问答详情
+@app.route('/detail/<question_id>/')
+def detail(question_id):
+    question_model = Question.query.filter(Question.id == question_id).first()
+    return render_template('detail.html', question=question_model)
 
 # 利用钩子函数判断登陆状态
 @app.context_processor
